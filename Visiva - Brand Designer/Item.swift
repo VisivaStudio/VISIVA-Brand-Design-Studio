8b7bb8116326ab6bf2172e1aff5945d0db3935c6
Visiva - Brand Design Studio <info@visiva.co.za>
2026-01-20 13:06:30 +0200

    Update Item.swift

--- Visiva - Brand Designer/Item.swift
+++ Visiva - Brand Designer/Item.swift
@@ -1,18 +1,7 @@
-//
-//  Item.swift
-//  Visiva - Brand Designer
-//
-//  Created by Visiva  - Brand Design Studio on 2026/01/04.
-//
-
 import Foundation
-import SwiftData
 
-@Model
-final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
+struct Item: Identifiable {
+    let id = UUID()
+    let title: String
+    let description: String
 }
