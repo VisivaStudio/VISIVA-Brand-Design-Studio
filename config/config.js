a54fbab54588fae550179c25164634d73389911d
Visiva - Brand Design Studio <info@visiva.co.za>
2026-02-03 09:34:04 +0200

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  APP_NAME: "VISIVA® Brand Design Platform",
+  SUPPORT_EMAIL: "info@visiva.co.za",
+  CDN_BASE: "https://cdn.visiva.co.za",
+  API_BASE: "", // add later
+};
